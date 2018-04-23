4b0e1b5c31cb7819b0defad58d483e78a2b23255
ysk_univ <ysk.univ.1007@gmail.com>
2018-04-23 15:29:21 +0900

    add test for Rational#==

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -6,6 +6,29 @@ describe Calculator::Factor do
 end
 
 describe Calculator::Rational do
+
+  describe '#==' do
+
+    before do
+      @ra = Calculator::Rational[1, 2]
+      @rb = Calculator::Rational[1, 2]
+      @rc = Calculator::Rational[2, 3]
+    end
+
+    context 'when an equal object is given' do
+      it 'returns true' do
+        expect(@ra == @rb).to be_truthy
+      end
+    end
+
+    context 'when a non-equal object is given' do
+      it 'returns false' do
+        expect(@ra == @rc).to be_falsey
+      end
+    end
+
+  end
+
 end
 
 describe Calculator::Radical do
