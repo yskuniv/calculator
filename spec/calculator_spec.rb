40fe7bd6750bd790b8e5670e5cef2e7bfd7461b7
ysk_univ <ysk.univ.1007@gmail.com>
2018-04-23 15:27:38 +0900

    prepare to test Calculator classes

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,2 +1,25 @@
 require 'spec_helper'
 require_relative '../calculator.rb'
+
+
+describe Calculator::Factor do
+end
+
+describe Calculator::Rational do
+end
+
+describe Calculator::Radical do
+end
+
+describe Calculator::Exponential do
+end
+
+describe Calculator::Logarithm do
+end
+
+
+describe Calculator::Term do
+end
+
+describe Calculator::Expression do
+end
