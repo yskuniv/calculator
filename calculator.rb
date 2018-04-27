a61bfa9322f530c4a543b07c47ea89cdc3911e23
ysk_univ <ysk.univ.1007@gmail.com>
2018-04-27 15:12:03 +0900

    fix Term#add

--- calculator.rb
+++ calculator.rb
@@ -204,6 +204,8 @@ module Calculator
     end
 
     def add(given)
+      return self.class.new(@factors) if given.nil?
+
       fcts_ = add_2factors(@factors, given.factors)
 
       self.class.new(*fcts_)
