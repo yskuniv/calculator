9dc7afefc6abbee5f0e336b33c1d01b4ecd4750a
ysk_univ <ysk.univ.1007@gmail.com>
2018-05-16 16:40:24 +0900

    impl Exponential.identity

--- calculator.rb
+++ calculator.rb
@@ -229,6 +229,10 @@ module Calculator
 
   class Exponential < PrimeFactor
     class << self
+      def identity
+        new(1, Rational.new(1, 1))
+      end
+
       def compare(a, b)
         [a.b, a.e] == [b.b, b.e]
       end
