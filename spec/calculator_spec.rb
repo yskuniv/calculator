187b8b04e26ae3831d57ad4fc0877703617ad687
ysk_univ <ysk.univ.1007@gmail.com>
2018-05-17 20:13:59 +0900

    fix to use aliases in all case

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -13,7 +13,7 @@ describe Calculator::Rational do
   describe '#simplify' do
 
     it 'simplifies properly' do
-      expect(Calculator::Rational[2, 4].simplify).to eq Calculator::Rational[1, 2]
+      expect(Rat[2, 4].simplify).to eq Rat[1, 2]
     end
 
   end
@@ -21,7 +21,7 @@ describe Calculator::Rational do
   describe '#multiply' do
 
     it 'multiplies with given properly' do
-      expect(Calculator::Rational[2, 3].multiply(Calculator::Rational[3, 4])).to eq Calculator::Rational[1, 2]
+      expect(Rat[2, 3].multiply(Rat[3, 4])).to eq Rat[1, 2]
     end
 
   end
@@ -29,7 +29,7 @@ describe Calculator::Rational do
   describe '#add' do
 
     it 'adds given properly' do
-      expect(Calculator::Rational[2, 3].add(Calculator::Rational[5, 6])).to eq Calculator::Rational[3, 2]
+      expect(Rat[2, 3].add(Rat[5, 6])).to eq Rat[3, 2]
     end
 
   end
@@ -46,14 +46,14 @@ describe Exponential do
 
     context 'when 1 is given as a base' do
       it 'returns the identity of Rational' do
-        expect(Exponential[1, Rat[2, 3]].simplify).
+        expect(Exp[1, Rat[2, 3]].simplify).
           to eq Rat[1, 1]
       end
     end
 
     context 'when an integer exponent is given' do
       it 'returns a simple Rational' do
-        expect(Exponential[2, Rat[3, 1]].simplify).
+        expect(Exp[2, Rat[3, 1]].simplify).
           to eq Rat[8, 1]
       end
     end
@@ -62,15 +62,15 @@ describe Exponential do
 
       context 'in case that only a Exponential is expected to return' do
         it 'returns a proper Exponential' do
-          expect(Exponential[2, Rat[2, 3]].simplify).
-            to eq Exponential[4, Rat[1, 3]]
+          expect(Exp[2, Rat[2, 3]].simplify).
+            to eq Exp[4, Rat[1, 3]]
         end
       end
 
       context 'in case that a CFactor is expected to return' do
         it 'returns a proper CFactor' do
-          expect(Exponential[24, Rat[1, 2]].simplify).
-            to eq CFactor[Rat[2, 1], Exponential[6, Rat[1, 2]]]
+          expect(Exp[24, Rat[1, 2]].simplify).
+            to eq CFactor[Rat[2, 1], Exp[6, Rat[1, 2]]]
         end
       end
 
@@ -80,15 +80,15 @@ describe Exponential do
 
       context 'when e.n < e.d' do
         it 'returns a proper CFactor' do
-          expect(Exponential[144, Rat[2, 3]].simplify).
-            to eq CFactor[Rat[12, 1], Exponential[12, Rat[1, 3]]]
+          expect(Exp[144, Rat[2, 3]].simplify).
+            to eq CFactor[Rat[12, 1], Exp[12, Rat[1, 3]]]
         end
       end
 
       context 'when e.n > e.d' do
         it 'returns a proper CFactor' do
-          expect(Exponential[144, Rat[8, 3]].simplify).
-            to eq CFactor[Rat[248832, 1], Exponential[12, Rat[1, 3]]]
+          expect(Exp[144, Rat[8, 3]].simplify).
+            to eq CFactor[Rat[248832, 1], Exp[12, Rat[1, 3]]]
         end
       end
 
@@ -100,14 +100,14 @@ describe Exponential do
 
     context 'in ordinary case' do
       it 'returns a correct value' do
-        expect(Exponential[2, Rat[1, 3]].multiply(Exponential[2, Rat[1, 3]])).
-          to eq Exponential[4, Rat[1, 3]]
+        expect(Exp[2, Rat[1, 3]].multiply(Exp[2, Rat[1, 3]])).
+          to eq Exp[4, Rat[1, 3]]
       end
     end
 
     context 'when an incompatible one is given' do
       it 'raises an Error' do
-        expect { Exponential[2, Rat[1, 3]].multiply(Exponential[3, Rat[1, 3]]) }.
+        expect { Exp[2, Rat[1, 3]].multiply(Exp[3, Rat[1, 3]]) }.
           to raise_error(Exponential::ExponentialMultiplicationError)
       end
     end
@@ -161,7 +161,7 @@ describe Expression do
     context 'when the term consists of Rationals' do
       it 'simplifies properly (This is not a simple unit test)' do
         expect(Expression[Term[Rat[2, 3], Rat[3, 4]],
-                                      Term[Rat[1, 3]]].simplify).
+                          Term[Rat[1, 3]]].simplify).
           to eq Expression[Term[Rat[5, 6]]]
       end
     end
