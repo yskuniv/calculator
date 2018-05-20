07b933f305c533325376de0626fd2475a8217221
ysk_univ <ysk.univ.1007@gmail.com>
2018-05-20 15:40:48 +0900

    fix indent

--- lib/calculator/calculator_metacode.rb
+++ lib/calculator/calculator_metacode.rb
@@ -1,76 +1,76 @@
 module Calculator
   module OperationDefinerHelper
-      def self.included(mod)
-        class << mod
-          def define_unary_operation(opr_name)
-            define_singleton_method(:included) do |cls|
-              cls.class_eval do
-                define_singleton_method(opr_name) do |a|
-                  raise NotImplementedError.new
-                end
+    def self.included(mod)
+      class << mod
+        def define_unary_operation(opr_name)
+          define_singleton_method(:included) do |cls|
+            cls.class_eval do
+              define_singleton_method(opr_name) do |a|
+                raise NotImplementedError.new
               end
             end
-
-
-            define_method(opr_name) do
-              self.class.method(opr_name)[self]
-            end
           end
 
-          def define_unary_operation_and_its_destructive_method(opr_name)
-            define_unary_operation(opr_name)
 
-            define_method("#{opr_name}!") do
-              elm_ = self.class.method(opr_name)[self]
+          define_method(opr_name) do
+            self.class.method(opr_name)[self]
+          end
+        end
 
-              destruct(elm_)
+        def define_unary_operation_and_its_destructive_method(opr_name)
+          define_unary_operation(opr_name)
 
-              self
-            end
+          define_method("#{opr_name}!") do
+            elm_ = self.class.method(opr_name)[self]
 
-            define_destruct
+            destruct(elm_)
+
+            self
           end
 
-          def define_binary_operation(opr_name)
-            define_singleton_method(:included) do |cls|
-              cls.class_eval do
-                define_singleton_method(opr_name) do |a, b|
-                  raise NotImplementedError.new
-                end
+          define_destruct
+        end
+
+        def define_binary_operation(opr_name)
+          define_singleton_method(:included) do |cls|
+            cls.class_eval do
+              define_singleton_method(opr_name) do |a, b|
+                raise NotImplementedError.new
               end
             end
-
-
-            define_method(opr_name) do |given|
-              raise OperandTypeMismatchError.new(self, given) unless self.class == given.class
-
-              self.class.method(opr_name)[self, given]
-            end
-          end
-
-          def define_binary_operation_and_its_destructive_method(opr_name)
-            define_binary_operation(opr_name)
-
-            define_method("#{opr_name}!") do |given|
-              elm_ = self.class.method(opr_name)[self, given]
-
-              destruct(elm_)
-
-              self
-            end
-
-            define_destruct
           end
 
 
-          private
+          define_method(opr_name) do |given|
+            raise OperandTypeMismatchError.new(self, given) unless self.class == given.class
 
-          def define_destruct
-            define_method(:destruct) do |elm_|
-              raise NotImplementedError.new
-            end
+            self.class.method(opr_name)[self, given]
+          end
+        end
+
+        def define_binary_operation_and_its_destructive_method(opr_name)
+          define_binary_operation(opr_name)
+
+          define_method("#{opr_name}!") do |given|
+            elm_ = self.class.method(opr_name)[self, given]
+
+            destruct(elm_)
+
+            self
+          end
+
+          define_destruct
+        end
+
+
+        private
+
+        def define_destruct
+          define_method(:destruct) do |elm_|
+            raise NotImplementedError.new
           end
         end
       end
+    end
   end
 end
