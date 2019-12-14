f31363b8da1c6dca3a8c06e08e6de95051e859fd
ysk_univ <ysk.univ.1007@gmail.com>
2019-12-15 00:51:27 +0900

    refactor around prime_numbers

--- lib/hs_math/utils.rb
+++ lib/hs_math/utils.rb
@@ -27,22 +27,27 @@ module HsMath
       end
 
       def prime_numbers
-        candidates = (2..Float::INFINITY)
-        founds = []
-
-        enum = Enumerator.new { |y|
-          candidates.each do |c|
-            (founds << c; y << c) if founds.all? { |f| c % f != 0 }
-          end
-        }
-
-        enum
+        (0..).lazy.map { |i| prime_number(i) }
       end
 
-      def is_prime_number?(x)
-        l = prime_numbers.take_while { |p| p <= x }
+      # define a memoization version of _prime_number()
+      proc {
+        cache = []
+        define_method :prime_number do |i|
+          cache[i] ||= _prime_number(i)
+        end
+      }[]
 
-        x == l.last
+      def _prime_number(i)
+        case i
+        when 0
+          2
+        else
+          preds = prime_numbers.take(i).force
+
+          ((preds.last + 1)..)
+            .find { |n| preds.all? { |p_| n % p_ != 0 } }
+        end
       end
     end
   end
