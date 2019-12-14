0bf0466b48e3f18d3681b9ddfacf5b9f2901fc7b
ysk_univ <ysk.univ.1007@gmail.com>
2019-12-15 00:56:32 +0900

    delete cases of removed methods

--- spec/hs_math/utils_spec.rb
+++ spec/hs_math/utils_spec.rb
@@ -36,18 +36,4 @@ RSpec.describe HsMath::Utils do
       expect(Utils.prime_numbers.take(10).force).to eq [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
     end
   end
-
-  describe "::is_prime_number?" do
-    context "when a prime number is given" do
-      it "returns true" do
-        expect(Utils.is_prime_number?(11)).to be_truthy
-      end
-    end
-
-    context "when a non-prime number is given" do
-      it "returns false" do
-        expect(Utils.is_prime_number?(12)).to be_falsey
-      end
-    end
-  end
 end
